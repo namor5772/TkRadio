e6a63fa9ed98ec3239ffdc37d9eb17f3a3bcb5b7
Roman M Groblicki <namor5772@gmail.com>
2025-06-08 20:36:36 +1000

    8/5/25 Desktop - a (working on station list)

--- Hardware/radio_Sucker.py
+++ Hardware/radio_Sucker.py
@@ -8,6 +8,7 @@ import requests
 import os
 import csv
 import re
+import sys
 
 from PIL import Image, ImageTk
 from tkinter import ttk
@@ -22,11 +23,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#sCore = "https://www.fmradiofree.com/?page=" # USD
-#sCore = "https://www.radio-australia.org/?page=" # AUSTRALIA"
-#sCore = "https://www.radio-polska.pl/?page=" # POLAND
-#sCore = "https://www.radio-polska.pl/?page=" # POLAND
-
 # Get the directory of the current script & then assumed directory for Images
 script_dir = os.path.dirname(os.path.abspath(__file__))
 script_dir = script_dir.replace("\\","/")
@@ -37,73 +33,108 @@ firefox_options = Options()
 # below is the headless width and height, if not headless +15 & 8 respectively
 firefox_options.add_argument("--width=1280")
 firefox_options.add_argument("--height=917")
-#firefox_options.add_argument("-headless")  # Ensure this argument is correct
+firefox_options.add_argument("-headless")  # Ensure this argument is correct
 browser = webdriver.Firefox(options=firefox_options)
 
 # 'cleans' browser between opening station websites
 refresh_http = "http://www.ri.com.au" # use my basic "empty" website
 
 
-# ***** VARIABLES NEEDED TO CUSTOMIZE THE SCRAPER *******
 
-sCore = "https://www.radio-uk.co.uk/?page=" # UK
-nPages = 61 # number of pages to scrape
-# Create the full filepath to the station websites text file
-filename = 'ukStationWebsites.csv' # filename of the station list csv file
-aCountry = "uk" # country code for the station list
-# *******************************************************
+# Create the full filepath to the countries websites csv file
+allcountries_filename = 'ALLCountries.csv' # filename of the countries list csv file
+allcountries_filepath = os.path.join(script_dir, allcountries_filename)
+print(f'The csv file {allcountries_filepath} stores a list of station websites')
+
+# load the countries list file into the 2D aCountries[] array
+aCountries = []
+with open(allcountries_filepath, mode="r", newline="", encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        aCountries.append(row)
+for row in aCountries:
+    print(row)
 
 
-# Create the full filepath to the station websites text file
-filepath = os.path.join(script_dir, filename)
-print(f'The csv file {filepath} stores a list of station websites')
+# Create the full filepath to csv file that will store all the radio station websites
+allstations_filename = 'ALL_StationWebsites.csv'
+allstations_filepath = os.path.join(script_dir, allstations_filename)
+print()
+print(f'The csv file {allstations_filepath} stores a list of station websites')
 total_anchor_tags = 0
 
+
 # Open or create a CSV file to append the scraped radio station results
-with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
-    for i in range (1,nPages+1):
-        time.sleep(3)
-        browser.get(refresh_http)
-        time.sleep(3)
-        sPath = sCore+str(i)
-        browser.get(sPath)
-        time.sleep(5)
+with open(allstations_filepath, "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
 
-        wait = WebDriverWait(browser, 10)
-        div_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "content-column")))
-        #print(div_element.text)
+    for row in aCountries:
+            
+        sCore = row[0] + "/?page=" # base URL for the station list
+        aCountry = row[1] # country code for the station list
+        sCountry = row[2] # country name for the station list
+        nPages = int(row[3]) # number of pages for the station list
+        continueFlag = True
+        textFlag = ""
+#        iPage = 1
 
-        # Find all <a> tags within that div
-        anchor_tags = div_element.find_elements(By.TAG_NAME, "a")
-        num_anchor_tags = len(anchor_tags)
+        for i in range (1,nPages+1):
+            time.sleep(2)
+            browser.get(refresh_http)
+            time.sleep(2)
+            sPath = sCore+str(i) # construct the URL for the current page
+            browser.get(sPath) # open the URL in the browser
+            time.sleep(5)
 
-        writer = csv.writer(csvfile)
-        for j, anchor in enumerate(anchor_tags):
-            if j >= 60:
-                j -= 1  
-                break 
-            anchor_text = anchor.text  
-            anchor_href = anchor.get_attribute('href')
+            # Wait for the page to load and find the div with class "content-column"
+            wait = WebDriverWait(browser, 10)
+            div_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "content-column")))
 
-            stationName = aCountry+" "+anchor_text
-            stationName = stationName.replace("/", "-")
-            stationName = stationName.replace("\\", "-")
-            stationName = stationName.replace(",", "-")
-            stationLogo = stationName.replace(" ", "_")
-            stationFunctionStr = "Commercial2"
-            nNum = 0
-            sPath2 = anchor_href
-            sClass = ""
-            nType = 0
-   
-            stationRow = [stationName, stationLogo, stationFunctionStr, nNum, sPath2, sClass, nType]
-            writer.writerow(stationRow)
-              # Write to CSV file
-            print(f"{stationRow}")
+            # Find all <a> tags within that div
+            anchor_tags = div_element.find_elements(By.TAG_NAME, "a")
+            num_anchor_tags = len(anchor_tags)
 
-        total_anchor_tags = total_anchor_tags + j+1
-        print(f"page: {i}, {j+1} links from page {sPath} have been written to {filepath}")
-print(f"**** A total of {total_anchor_tags} links have been written to {filepath}")
+            # extracting information for up to 60 stations from the openend web page
+            # There might be more but the extra ones (if any) are part of a list of interesting links 
+            for j, anchor in enumerate(anchor_tags):
+                if j >= 60:
+                    j -= 1  
+                    break 
+                anchor_text = anchor.text  
+                anchor_href = anchor.get_attribute('href')
+                stationName = aCountry+" "+anchor_text
+                stationName = stationName.translate(str.maketrans({char: "-" for char in "\\/,."}))
+                stationLogo = stationName.replace(" ", "_")
+                stationFunctionStr = "Commercial2"
+                nNum = 0
+                sPath2 = anchor_href
+                sClass = sCountry
+                nType = 0
+
+
+                # get anchor text for the first station on the first page
+                # this should repeat when you loop back to the first page
+#                    if iPage == 1 and j == 0:
+#                        textFlag = anchor_text
+
+                # if the anchor text is the same as the first station, stop scraping
+#                    if anchor_text == textFlag and iPage > 1:
+#                        print(f"Stopped on page {iPage}, this is the same page as the first one, so no more links where scraped from there!")
+#                        continueFlag = False
+#                        break    
+#                    else:
+                # append the station information to the file    
+                stationRow = [stationName, stationLogo, stationFunctionStr, nNum, sPath2, sClass, nType]
+                writer.writerow(stationRow)
+                #print(f"{stationRow}")
+
+            # we are past the last page (as indicated by the continueFlag) then doe not increment the page number etc.
+#                if continueFlag:
+            total_anchor_tags = total_anchor_tags + j+1
+            print(f"page: {i}, {j+1} links from page {sPath} have been written to {allstations_filepath}")
+
+        print(f"**** A total of {total_anchor_tags} links have been written to {allstations_filepath}")
+
 
 print("Press Ctrl+C to stop.")
 try:
@@ -112,20 +143,3 @@ try:
 except KeyboardInterrupt:
     print("Closing the app...")
     browser.quit() # close the WebDriver
-
-# 2D array of preset radio stations, in long name and index (to aStation[]) format.
-# this is the default, but is actually copied from file at statup and saved to file on exit!
-aStation = []
-for row in aStationCSV:
-    stationName = row[0]
-    stationName = stationName.replace("/", "v")
-    stationName = stationName.replace(",", "")
-    stationLogo = stationName.replace(" ", "_")
-    nNum = 0
-    sPath = row[1]
-    sClass = ""
-    nType = 0
-    station = [stationName, stationLogo, Commercial2, nNum, sPath, sClass, nType]
-    aStation.append(station)
-for row in aStation:
-    print(row)
